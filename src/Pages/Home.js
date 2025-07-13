e55dcf7f92c562dab6a679a836215babf7d2424d
Tarun Rawal <tarunrawal100@gmail.com>
2025-07-13 15:25:18 +0530

    fixed issues

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import LOGO from "../assets/search.jpg";
 import { toast } from "react-toastify";
 import { useSearchContext } from "../context/SearchContext";
 import InfiniteScroll from "react-infinite-scroll-component";
